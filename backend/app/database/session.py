306f27f1f03de8ca468e159e1219104500ba4c41
Matrix-X <hbj418@gmail.com>
2024-12-30 17:00:43 +0800

    feat(db): application name for psycopg

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -17,11 +17,6 @@ def get_database_sync_url():
 COMMON_CONFIG = {
     "pool_recycle": settings.database.pool_recycle,  # 连接最大生命周期（秒）
     "pool_pre_ping": settings.database.pool_pre_ping,
-    "connect_args":{
-        "server_settings": {
-            "application_name": "brainx"  # Correct way to add application_name for asyncpg
-        }
-    },
 }
 
 async_db_engine = create_async_engine(
@@ -30,6 +25,11 @@ async_db_engine = create_async_engine(
     max_overflow=settings.database.max_overflow,
     pool_timeout=settings.database.pool_timeout,  # 连接池获取连接的超时时间（秒）
     echo=settings.database.echo,  # 输出 SQL 日志
+    connect_args={
+        "server_settings": {
+            "application_name": "brainx"  # Correct way to add application_name for asyncpg
+        }
+    },
     **COMMON_CONFIG  # 传递通用配置
 )
 
@@ -44,6 +44,7 @@ async_session_local = async_sessionmaker(
 sync_engine = create_engine(
     get_database_sync_url(),  # 这里用你的实际数据库 URL
     poolclass=NullPool,  # 明确指定 NullPool 防止同步连接池复用连接
+    connect_args={"application_name": "brainx"},
     **COMMON_CONFIG  # 传递通用配置
 )
 
