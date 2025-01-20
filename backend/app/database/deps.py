47cb75c90e55a8482e90a2d7c73deb6a5d1bd4f3
Matrix-X <hbj418@gmail.com>
2025-01-20 11:02:18 +0800

    refact(db): change session with log

--- backend/app/database/deps.py
+++ backend/app/database/deps.py
@@ -12,33 +12,40 @@ from app.database.session import async_session_local, sync_session_local
 
 async def get_async_db_session() -> AsyncSession:
     from app.api.context_manager import context_set_db_session_rollback
+
     async with async_session_local() as db:
-        await db.execute(text(f"SET search_path TO {settings.database.db_schema}, public"))
+        await db.execute(
+            text(f"SET search_path TO {settings.database.db_schema}, public")
+        )
 
         try:
             yield db
             #  commit the db session if no exception occurs
             #  if context_set_db_session_rollback is set to True then rollback the db session
             if context_set_db_session_rollback.get():
-                logger.info('rollback db session')
+                logger.info("Rollback Async DB session")
                 await db.rollback()
             else:
                 await db.commit()
         except Exception as e:
             #  rollback the db session if any exception occurs
-            logger.error(f"session local error: {e}")
+            logger.error(f"Asyanc Session local error: {e}")
             await db.rollback()
             raise e
 
-        finally:
-            #  close the db session
-            await db.close()
+        # No need for `await db.close()` since context manager handles it
+        # finally:
+        #  close the db session
+        # await db.close()
 
 
 @contextmanager
 # 为了某些场景，比如 Celery 等需要直接调用的场景，你可以使用一个简单的函数来获取 session：
 def get_sync_db_session() -> Session:
     db = sync_session_local()
+    if not db:
+        raise ValueError("Failed to initialize sync session")
+
     db.execute(text(f"SET search_path TO {settings.database.db_schema}, public"))
     # 外层一定要用 with 才能让这个上下文管理器的功能生效。
     # 如果直接调用 get_sync_db_session() 而不使用 with，except 和 finally 块中的代码将不会被执行，
@@ -48,17 +55,16 @@ def get_sync_db_session() -> Session:
     try:
         yield db
         # 提交事务
-        if db:
-            print("sync db commit")
-            db.commit()
+
+        logger.info("Sync DB commit")
+        db.commit()
     except Exception as e:
         # 出现异常时回滚事务
-        if db:
-            print("sync db rollback")
-            db.rollback()
+        logger.error("Sync DB rollback")
+        db.rollback()
         raise e
     finally:
         # 关闭数据库会话
-        if db:
-            print("sync db closed")
-            db.close()
+
+        logger.info("Sync DB session closed")
+        db.close()
