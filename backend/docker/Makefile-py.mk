1b23fd19062f33041eacedbf9f9b4f4d5b83cc80
Matrix-X <hbj418@gmail.com>
2025-02-18 16:35:04 +0800

    update

--- backend/docker/Makefile-py.mk
+++ backend/docker/Makefile-py.mk
@@ -17,8 +17,8 @@ app-run:
 
 task-run:
 	echo "Running celery task in production mode."
-	celery -A app.service.task.celery_worker worker -n celery@worker_task --loglevel=info -P gevent
+	celery -A app.service.task.celery_worker worker -n brainx@worker_task --loglevel=info -P gevent
 
 task-rag-run:
 	echo "Running celery rag task in production mode."
-	celery -A app.service.task.celery_worker worker -Q rag_queue -n celery@worker_rag --loglevel=info -P gevent
+	celery -A app.service.task.celery_worker worker -Q rag_queue -n brainx@worker_rag --loglevel=info -P gevent
